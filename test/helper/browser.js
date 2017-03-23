1e2db35e0fe48257e4416167c9611e4244bd3879
Shin Cheng Lin <PriaseMacbookPro@Shins-MacBook-Pro.local>
2017-03-23 23:48:14 +0800

    using spy success

--- test/helper/browser.js
+++ test/helper/browser.js
@@ -3,12 +3,8 @@ require('babel-register')();
 var ReactTestUtils = require('react-addons-test-utils')
 var jsdom = require('jsdom').jsdom;
 
-//var _ = require('lodash');
-
 var exposedProperties = ['window', 'navigator', 'document', '_'];
-
 global._ = require('lodash');
- 
 global.document = jsdom('');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
